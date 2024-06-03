429ab2c504f90e4db15c2272e901cfe49b64204d
dhruvakhariaa <vakhariadhruv526@gmail.com>
2024-06-03 08:09:32 +0530

    Code for checking whether number is prime or not

---
+++ isPrime.java
@@ -0,0 +1,25 @@
+public class isPrime 
+{
+    public static void main(String[] args) {
+        int number = 29; // Change this number to check for primality
+
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
