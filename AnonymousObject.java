8847a02712da2bd4d3927dabb7590629f13063b6
dhruvakhariaa <vakhariadhruv526@gmail.com>
2024-12-07 11:28:05 +0530

    Example of anonymous object

---
+++ AnonymousObject.java
@@ -0,0 +1,17 @@
+class A{
+
+    A(){
+        System.out.println("Constructor");
+    }
+
+    public void show(){
+        System.out.println("Hello World");
+    }
+}
+
+public class AnonymousObject {
+    public static void main(String[] args) {
+        new A(); // creating object of A and this object can be used only once 
+        new A().show(); // creating object of A and calling show method
+    }    
+}
